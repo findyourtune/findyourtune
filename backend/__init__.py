4af6dc59feb9c4b86b57150259b91fb5bc42bb2c
Matthew Jackson <matthewjackson6118@gmail.com>
2021-01-14 15:20:24 -0500

    initial skeleton for flask be

---
+++ backend/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from backend.config import Config
+
+
+db = SQLAlchemy()
+# bcrypt = Bcrypt()
+# login_manager = LoginManager()
+# login_manager.login_view = 'users.login'
+# login_manager.login_message_category = 'info'
+# mail = Mail()
+bootstrap = Bootstrap()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from backend.home.routes import main
+    app.register_blueprint(main)
+
+    return app
---
+++ backend/home/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint, jsonify
+from flask_wtf import Form
+from wtforms.fields.html5 import DateField
+from backend import db
+from backend.models import User
+# from flask_login import current_user
+# from datetime import datetime, date
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+def home():
+    return 'Hello World'
