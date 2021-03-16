1a4032a3f3e73d5ee421811e3648445fe8971c95
Stephen Comarata <comarasj@mail.uc.edu>
2021-03-16 16:04:11 -0400

    Suggested People

--- frontend/src/components/SuggestedPeople.vue
+++ frontend/src/components/SuggestedPeople.vue
@@ -3,7 +3,20 @@
     <b-card>
       <b-row>
         <b-col>
-          <div>{{person.user_firstname}}</div>
+          <div @click="goToUser()" class="user-page-link-button">
+            <b-avatar
+              :style="{ 'background-color': appColor }"
+              variant="info"
+              :text="getAvatarText()"
+              size="3.5rem"
+            ></b-avatar>
+            <span class="user-info">
+              <div class="user-display-name">
+                {{ person.user_firstname }} {{ person.user_lastname }}
+              </div>
+              <div class="user-username">@{{ person.user_username }}</div>
+            </span>
+          </div>
         </b-col>
       </b-row>
     </b-card>
@@ -18,11 +31,45 @@ export default {
       type: Object,
     },
   },
+  computed: {
+    profileLink() {
+      return "/u/" + this.person.user_username;
+    },
+  },
+  methods: {
+    getAvatarText() {
+      try {
+        let firstInitial = this.person.user_firstname.charAt(0);
+        let lastInitial = this.person.user_lastname.charAt(0);
+        return firstInitial.toUpperCase() + lastInitial.toUpperCase();
+      } catch (err) {
+        return "";
+      }
+    },
+    goToUser() {
+      this.$router.push(this.profileLink);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .suggested-people-card {
   padding-bottom: 10px;
+}
+
+.user-info {
+    vertical-align: middle;
+    display: inline-block;
+    padding-left: 15px;
+}
+.user-display-name {
+    font-size: 1.2rem;
+    font-style: bold;
+    display: inline-block;
+}
+.user-username {
+    font-size: 14px;
+    font-style: italic;
 }
 </style>
